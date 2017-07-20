fa5026268b592f9d55adc7e900afd23801cd5424
lfarant <lfarant01@gmail.com>
2017-07-20 16:27:41 +0200

    add route view

--- src/Trp/MdlBundle/Controller/MdlController.php
+++ src/Trp/MdlBundle/Controller/MdlController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class MdlController extends Controller
 {
+	public function viewAction($id){
+		
+		return new Response("Affichage de l'annonce d'id : ".$id);
+	}
+	
     public function indexAction()
     {
 		$content = $this->get('templating')->render('TrpMdlBundle:Mdl:index.html.twig');
