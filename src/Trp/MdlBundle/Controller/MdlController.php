b942c972e6421127811b3a6ed97f8ef1bb0fdf50
lfarant <lfarant01@gmail.com>
2017-07-21 03:35:52 +0200

    add step1 + btn link

--- src/Trp/MdlBundle/Controller/MdlController.php
+++ src/Trp/MdlBundle/Controller/MdlController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class MdlController extends Controller
 {
-	public function viewAction($id){
+	public function stepAction($id){
 		
-		return new Response("Affichage de l'annonce d'id : ".$id);
+		$content = $this->get('templating')->render("TrpMdlBundle:Mdl:step$id.html.twig");
+    
+    	return new Response($content);
 	}
 	
     public function indexAction()
